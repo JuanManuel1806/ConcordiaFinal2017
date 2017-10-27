cdb1e4ded34beca20c6400b75aa14907e4ddc29e
Juan Manuel <manuel.jimenez@udea.edu.co>
2017-10-27 08:01:02 -0500

    TRABAJO INFORMATICA AVANZADA

---
+++ Concordia/Controllers/MisUsuariosController.cs
@@ -0,0 +1,57 @@
+﻿using Concordia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace concordia.Controllers
+{
+    public class MisUsuariosController : Controller
+    {
+        //
+        // GET: /MisUsuarios/Usuarios
+
+        public ActionResult Usuarios()
+        {
+            return View();
+        }
+        //
+        // GET: /MisUsuarios/Medicos
+
+        public ActionResult Médicos()
+        {
+            return View();
+        }
+
+        //
+        // GET: /MisUsuarios/Agenda
+        public ActionResult Agenda()
+        {
+            return View();
+        }
+
+        //POST: /Usuario/Create/USANDOAJAX
+        [HttpPost]
+        public JsonResult CrearAJAX(Usuario usuario)
+        {
+            try
+            {
+                var json = Json(new { mensaje = "" });
+                return json;
+            }
+            catch (Exception ex)
+            {
+                return Json(new { mensaje = ex.Message });
+            }
+        }
+        //POST: /Usuario/Create/USANDOASUBMITTRADICIONAL
+
+        [HttpPost]
+        public ActionResult Crear(Usuario usuario)
+        {
+            return View();
+
+        }
+    }
+}
---
+++ Concordia/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Concordia.Models
+{
+    public class Usuario
+    {
+        public string tipodedocumento { get; set; }
+         public string numerodedocumento { get; set; }
+         public string primernombre { get; set; }
+         public string segundonombre { get; set; }
+         public string primerapellido { get; set; }
+         public string segundoapellido { get; set; }
+         public DateTime fechadenacimiento { get; set; }
+}
