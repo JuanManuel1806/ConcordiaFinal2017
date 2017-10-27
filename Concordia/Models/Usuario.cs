cbd8da91aad0f850ff2d311af635fba2b0940540
Juan Manuel <manuel.jimenez@udea.edu.co>
2017-10-27 12:40:01 -0500

    Trabajo Informatica Avanzada

--- Concordia/Models/Usuario.cs
+++ Concordia/Models/Usuario.cs
@@ -8,10 +8,11 @@ namespace Concordia.Models
     public class Usuario
     {
         public string tipodedocumento { get; set; }
-         public string numerodedocumento { get; set; }
-         public string primernombre { get; set; }
-         public string segundonombre { get; set; }
-         public string primerapellido { get; set; }
-         public string segundoapellido { get; set; }
-         public DateTime fechadenacimiento { get; set; }
+        public string numerodedocumento { get; set; }
+        public string primernombre { get; set; }
+        public string segundonombre { get; set; }
+        public string primerapellido { get; set; }
+        public string segundoapellido { get; set; }
+        public DateTime fechadenacimiento { get; set; }
+    }
 }
